7ee62ecee6becb6b371baa638c2db8d8daafae11
ersinev <ersinevci@hotmail.com>
2023-08-24 15:53:01 +0200

    updated

--- src/MonthlyChart.js
+++ src/MonthlyChart.js
@@ -1,23 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { Modal, Container, Row, Col } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { Modal, Container, Row, Col } from "react-bootstrap";
 //import SimpleAreaChart from './altCharts/SimpleAreaChart';
 //import SimpleFunnelChart from './altCharts/SimpleFunnelChart';
 //import SimpleStreamChart from './altCharts/SimpleStreamChart';
 //import MyResponsiveAreaBump from './altCharts/MyResponsiveAreaBump';
-import SimpleBarChart from './altCharts/SimpleBarChart';
-import CalendarChart from './altCharts/CalendarChart';
-import SimplePieChart from './altCharts/SimplePieChart';
-
-
-
-
+import SimpleBarChart from "./altCharts/SimpleBarChart";
+import CalendarChart from "./altCharts/CalendarChart";
+import SimplePieChart from "./altCharts/SimplePieChart";
 
 function MonthlyChart({ data, showModal, handleClose, capacity }) {
   const [chartData, setChartData] = useState(null);
-  
 
   useEffect(() => {
-    setChartData(null); 
+    setChartData(null);
 
     if (data) {
       const fetchData = async () => {
@@ -34,21 +29,28 @@ function MonthlyChart({ data, showModal, handleClose, capacity }) {
           if (response.ok) {
             const responseData = await response.json();
             if (responseData && responseData.length > 0) {
-              setChartData(responseData.map(item => {
-                return {
-                  uv: Math.round(parseFloat(item.value)),
-                  date: item.created_at.split('T')[0],
-                  className: data.className
-                };
-              }));
+              setChartData(
+                responseData.map((item) => {
+                  return {
+                    uv: Math.round(parseFloat(item.value)),
+                    date: item.created_at.split("T")[0],
+                    className: data.className,
+                  };
+                })
+              );
             } else {
               console.warn("No data received from API.");
             }
           } else {
-            console.warn(`Failed fetching for ${data.adafruitUsername}/${data.feedKey}`);
+            console.warn(
+              `Failed fetching for ${data.adafruitUsername}/${data.feedKey}`
+            );
           }
         } catch (error) {
-          console.warn(`Error fetching or processing data for ${data.adafruitUsername}/${data.feedKey}:`, error);
+          console.warn(
+            `Error fetching or processing data for ${data.adafruitUsername}/${data.feedKey}:`,
+            error
+          );
         }
       };
 
@@ -66,22 +68,20 @@ function MonthlyChart({ data, showModal, handleClose, capacity }) {
           <Container fluid>
             <Row>
               <Col md={12}>
-                <CalendarChart  chartData={chartData} />
+                <CalendarChart chartData={chartData} />
               </Col>
-             
             </Row>
             <Row>
               <Col md={7}>
-              <SimplePieChart chartData={chartData} />
+                <SimplePieChart chartData={chartData} />
               </Col>
               <Col md={5}>
                 <SimpleBarChart chartData={chartData} />
               </Col>
-             
             </Row>
           </Container>
         ) : (
-          <p>Loading...</p>  // You can replace this with a spinner or any loading animation if you prefer.
+          <p>Loading...</p> // You can replace this with a spinner or any loading animation if you prefer.
         )}
       </Modal.Body>
     </Modal>
