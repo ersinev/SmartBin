d7c3d6f36730345e9db9748b5a760b33531f9d11
ersinev <ersinevci@hotmail.com>
2023-09-29 10:49:41 +0200

    updated

--- src/WeightData.js
+++ src/WeightData.js
@@ -25,6 +25,7 @@ const WeightData = ({
           }
         );
         const data = await response.json();
+        console.log(data)
 
         if (data.length > 0) {
           const latestData = data[0];
